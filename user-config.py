92bfff9774f3176d76776826ad22cf08d92191c5
Max Semenik <maxsem.wiki@gmail.com>
2018-05-08 21:47:13 -0700

    Add user-config.py to VCS
    Less things to set up to get the bot running,
    gives an uniform way of configuring user credentials.
    
    Bug: T194214

---
+++ user-config.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# ****************************************
+# * Pywikibot settings file for this bot *
+# ****************************************
+
+# Required by Pywikibot, but not needed for anything we use
+mylang = 'en'
+family = 'wikipedia'
+
+# Username across the cluster
+usernames['wikipedia']['*'] = \
+    usernames['wikibooks']['*'] = \
+    usernames['wikinews']['*'] = \
+    usernames['wikiquote']['*'] = \
+    usernames['wikisource']['*'] = \
+    usernames['wikiversity']['*'] = \
+    usernames['test']['*'] = \
+    'MaxSem test bot'
+
+# Secret password file, see
+# https://www.mediawiki.org/wiki/Manual:Pywikibot/BotPasswords
+# for instructions
+password_file = 'user-password.py'
+
+# Setting this to True generates lots of debug output
+verbose_output = True
