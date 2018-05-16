11730fc40bec7e8f74579b4b57e4be2c7b632168
Max Semenik <maxsem.wiki@gmail.com>
2018-05-15 19:54:26 -0700

    Add integration scripts
    Not tested thoroughly, will do so son.
    
    Bug: T194214

---
+++ bin/vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This file sets up some common variables for other scripts in this directory
+
+set -e
+
+USER=s53689
+DB=${USER}__commonsbot
+DIR=$(dirname "$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")")
+TIMESTAMP=$(date '+%Y%m%d%H%M%S')
