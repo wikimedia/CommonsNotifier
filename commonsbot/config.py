ee5b0712931d829be7627458935f69e5532854aa
Max Semenik <maxsem.wiki@gmail.com>
2018-05-18 15:08:57 -0700

    Open configs relatively to the bot root directory

--- commonsbot/config.py
+++ commonsbot/config.py
@@ -1,7 +1,9 @@
 import json
 import os
 
-file = open('config.json', 'rt')
+basedir = os.path.dirname(os.path.dirname(__file__)) + '/'
+
+file = open(basedir + 'config.json', 'rt')
 settings = json.loads(file.read())
 file.close()
 db_connections = settings['db_connections']
@@ -10,12 +12,12 @@ dry_run = False
 if 'dry-run' in settings:
     dry_run = settings['dry-run']
 
-file = open('wikis-enabled', 'r')
+file = open(basedir + 'wikis-enabled', 'r')
 wikis = set([s.strip() for s in file.readlines()])
 file.close()
 
 mysql_config_file = None
-for dir in ['.', os.environ['HOME']]:
+for dir in ['.', basedir, os.environ['HOME']]:
     for file in ['my.cnf','replica.my.cnf']:
         file = '%s/%s' % (dir, file)
         if os.path.isfile(file):
