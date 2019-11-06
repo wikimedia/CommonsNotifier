fe3c75fab3467439630e4604d5ed1ad7d86ee574
Max Semenik <maxsem.wiki@gmail.com>
2019-11-06 15:31:15 -0800

    Error handling

--- bin/vars.sh
+++ bin/vars.sh
@@ -13,3 +13,18 @@ DB=${USER}__commonsbot
 DIR=$(dirname "$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")")
 BIN="${DIR}/virtualenv/bin"
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
+EMAIL_USER="commtech-commons"
+EMAIL_MAILBOX="alerts"
+EMAIL_SERVER="tools.wmflabs.org"
+ERROR_EMAIL="${EMAIL_USER}.${EMAIL_MAILBOX}@${EMAIL_SERVER}"
+ERROR_LINES=30
+
+
+handle_error() {
+    echo "Cronjob ${TIMESTAMP} failed at $(date)"
+    echo "Emailing the maintainers"
+    jsub -quiet -once ${DIR}/bin/report-error
+    exit 1
+}
+
+trap "handle_error" ERR
