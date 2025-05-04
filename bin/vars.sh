053aa8de3f5b0a11116a732d99595dbab4defc53
MusikAnimal <MusikAnimal@users.noreply.github.com>
2025-05-04 04:34:07 -0400

    Update vars.sh
    update email server and venv path

--- bin/vars.sh
+++ bin/vars.sh
@@ -11,20 +11,10 @@ export LC_CTYPE=en_US.UTF-8
 USER=s53689
 DB=${USER}__commonsbot
 DIR=$(dirname "$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")")
-BIN="${DIR}/virtualenv/bin"
+BIN="${DIR}/pyvenv/bin"
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 EMAIL_USER="commtech-commons"
 EMAIL_MAILBOX="alerts"
-EMAIL_SERVER="tools.wmflabs.org"
+EMAIL_SERVER="toolforge.org"
 ERROR_EMAIL="${EMAIL_USER}.${EMAIL_MAILBOX}@${EMAIL_SERVER}"
 ERROR_LINES=30
-
-
-handle_error() {
-    echo "Cronjob ${TIMESTAMP} failed at $(date)"
-    echo "Emailing the maintainers"
-    jsub -quiet -once ${DIR}/bin/report-error
-    exit 1
-}
-
-trap "handle_error" ERR
